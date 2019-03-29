8be7e287a18d4f0e0563d441f192685d1e41f173
Masagis <31729034+Masagis@users.noreply.github.com>
2019-03-30 05:57:29 +0700

    update sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -61,17 +61,6 @@ $queryMenu=   "SELECT `user_menu`.`id`, `menu`
 <!-- endforeach -->
 
 
-<!-- Nav Item - Logout -->
-<li class="nav-item">
-  <a class="nav-link" href="<?= base_url('auth/logout');?> ">
-    <i class="fas fa-fw fa-sign-out-alt"></i>
-    <span>Logout </span></a>
-</li>
-
-
-<!-- Divider -->
-<hr class="sidebar-divider d-none d-md-block">
-
 <!-- Sidebar Toggler (Sidebar) -->
 <div class="text-center d-none d-md-inline">
   <button class="rounded-circle border-0" id="sidebarToggle"></button>
