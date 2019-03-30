caf660ba0e2d52ffd1c611c77800efa523e3ad3c
Masagis <31729034+Masagis@users.noreply.github.com>
2019-03-30 17:53:34 +0700

    add account verify

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -75,9 +75,11 @@ class User extends CI_Controller {
     {
         $data['title'] = 'Change Password';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        
         $this->form_validation->set_rules('current_password', 'Current Password', 'required|trim');
         $this->form_validation->set_rules('new_password1', 'New Password', 'required|trim|min_length[3]|matches[new_password2]');
         $this->form_validation->set_rules('new_password2', 'Confirm New Password', 'required|trim|min_length[3]|matches[new_password1]');
+        
         if ($this->form_validation->run() == false) {
             $this->load->view('templates/header', $data);
             $this->load->view('templates/sidebar', $data);
@@ -97,9 +99,11 @@ class User extends CI_Controller {
                 } else {
                     // password sudah ok
                     $password_hash = password_hash($new_password, PASSWORD_DEFAULT);
+                    
                     $this->db->set('password', $password_hash);
                     $this->db->where('email', $this->session->userdata('email'));
                     $this->db->update('user');
+                    
                     $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Password changed!</div>');
                     redirect('user/changepassword');
                 }
