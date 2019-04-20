e05ce52044733ffb22aadf496004b985c79a20ca
vallen <vallenrefkiyawan123@gmail.com>
2019-04-20 21:10:41 +0700

    updet daftar

--- application/views/mahasiswa/daftar.php
+++ application/views/mahasiswa/daftar.php
@@ -4,6 +4,25 @@
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
+    <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Semester
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">1</a>
+    <a class="dropdown-item" href="#">2</a>
+    <a class="dropdown-item" href="#">3</a>
+    <a class="dropdown-item" href="#">4</a>
+    <a class="dropdown-item" href="#">5</a>
+    <a class="dropdown-item" href="#">6</a>
+    <a class="dropdown-item" href="#">7</a>
+    <a class="dropdown-item" href="#">8</a>
+    
+  </div>
+</div>
+
+
+
     </div>
     <!-- /.container-fluid -->
 
