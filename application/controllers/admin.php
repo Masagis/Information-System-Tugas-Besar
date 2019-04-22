31020b99e389d1734f009577b7f16bd86184d2b2
Doni Agus Adila <pangerancleeper@gmail.com>
2019-04-22 14:44:27 +0700

    Update admin.php

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -69,4 +69,12 @@ class admin extends CI_Controller {
         $this->session->set_flashdata('message','<div class="alert alert-success" role ="alert">
         Access Change! </div> ');
     }
+    public function deleteSubMenu($id)
+    {
+        $this->db->delete('user_sub_menu', ['id' => $id]);
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role ="alert">
+        Data Dihapus! </div>');
+        redirect('menu/submenu');
+    }
+     
 }
