df874d90542723d9e9343745f7add77f158a8923
Elygea30 <Elygea30@gmail.com>
2019-04-23 06:34:02 +0700

    Update pengumuman.php

--- application/views/informasi/pengumuman.php
+++ application/views/informasi/pengumuman.php
@@ -2,12 +2,13 @@
 <div class="container-fluid">
 
     <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">
     <p>Pengumuman Tugas Besar</p>
-    <p><a href="#"class="text-decoration-none">1.Jadwal Pelaksanaan Tugas Besar Teknik Sipil</a></p>
-    <p><a href="#"class="text-decoration-none">2.Spesifikasi Tugas Besar Teknik Sipil</a></p>
-    <p><a href="#"class="text-decoration-none">3.Jadwal Pengumpulan Tugas Besar Teknik Sipil</a></p>
-    <p><a href="#"class="text-decoration-none">4.Informasi Asisten Tugas Besar Teknik Sipil</a></p>
-    <p><a href="#"class="text-decoration-none">5.Informasi Kelompok Tugas Besar Teknik Sipil</a></p>
+
+    <h5 class="h5 mb-2 text-gray-800">
+    <p><a href="#"class="text-decoration-none">1.Spesifikasi Tugas Besar Teknik Sipil</a></p>
+    <p><a href="#"class="text-decoration-none">2.Jadwal Pengumpulan Tugas Besar Teknik Sipil</a></p>
+    <p><a href="#"class="text-decoration-none">3.Informasi Asisten Tugas Besar Teknik Sipil</a></p>
     </div>
     <!-- /.container-fluid -->
 
