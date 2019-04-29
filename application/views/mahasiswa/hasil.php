59d106a8908c8383234971af411b6d19b1d530fe
TioFazri <tiofajri@yahoo.co.id>
2019-04-29 16:26:21 +0700

    Update hasil.php

--- application/views/mahasiswa/hasil.php
+++ application/views/mahasiswa/hasil.php
@@ -15,20 +15,27 @@
 
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
-<div class="center">
-<select>
-  <option value="Tubes1">Tubes1</option>
-  <option value="Tubes2">Tubes1</option>
-  <option value="Tubes3">Tubes1</option>
-  <option value="Tubes4">Tubes1</option>
-</select>
+<div class = "form-group row">
+  <label class = "col-sm-2 col-form-label"> Tingkat semester </label>
+      <div class="">
+        <select>
+          <option value="">--Pilih--</option>
+          <option value="Tubes1">Tubes1</option>
+          <option value="Tubes2">Tubes2</option>
+          <option value="Tubes3">Tubes3</option>
+          <option value="Tubes4">Tubes4</option>
+        </select>
+      </div>
 </div>
 
 <br>
-<div class="center">
-<button type="submit" class="btn btn-primary">download</button>
+
+<div class="form-group row justify-content-end">
+    <div class="col-sm-10">
+        <button type="submit" class="btn btn-primary">download</button>
+     </div>
 </div>
-    </div>
+
     <!-- /.container-fluid -->
 
 </div>
