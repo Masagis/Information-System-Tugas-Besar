a8aab01f3f84acded5799ff30c163affaf52f466
wicak <ariftheaxr@gmail.com>
2019-04-30 16:37:19 -0700

    kelompok_model
    membuat model kelompok untuk menguji data anggota yang daftar daru tabel baru 'Anggota'

---
+++ application/models/Kelompok_model.php
@@ -0,0 +1,12 @@
+<?php
+ defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+ class Kelompok_model extends CI_Model {
+ 
+     public function getAnggota(){ 
+         //mengambil data daftar mahasiswa dari table anggota   
+          return $this->db->get('anggota')->result();
+     }
+
+ }
+ 
