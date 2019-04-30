725ff23525ba7aba8565a282cbada287c5dc42a8
wicak <ariftheaxr@gmail.com>
2019-04-30 16:39:38 -0700

    view export.php
    tampilan untuk menu export  ke excel daftar mahasiswa yang daftar

---
+++ application/views/Kelompok/Eksport.php
@@ -0,0 +1,12 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+    <a href="<?php echo base_url("index.php/Kelompok/export"); ?>">Export ke Excel</a><br><br>
+   
+    <!-- /.container-fluid -->
+    </div>
+</div>
+<!-- End of Main Content -->
