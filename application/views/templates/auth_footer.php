dd2ada8a5277dc89d361a83b4609f8ec10d88269
Masagis <31729034+Masagis@users.noreply.github.com>
2019-05-05 03:23:27 +0700

    update footer & header

--- application/views/templates/auth_footer.php
+++ application/views/templates/auth_footer.php
@@ -8,6 +8,10 @@
     <!-- Custom scripts for all pages-->
     <script src="<?= base_url('assets/');?>js/sb-admin-2.min.js"></script>
 
+    <!-- Plugin Datatables-->
+    <script type="text/javascript" src="<?= base_url('assets/');?>js/jquery.dataTables.js"></script>
+    <script type="text/javascript" src="<?= base_url('assets/');?>js/dataTables.bootstrap4.js"></script>
+
 </body>
 
 </html>
