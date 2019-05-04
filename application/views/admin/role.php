3e49abb444890b34fe3f55202e3870092b16daf3
Masagis <31729034+Masagis@users.noreply.github.com>
2019-05-05 03:25:32 +0700

    delete code

--- application/views/admin/role.php
+++ application/views/admin/role.php
@@ -27,9 +27,7 @@
                 <th scope="row"><?= $i;?> </th>
                 <td><?= $r['role'];?></td>
                 <td>
-                    <a href="<?= base_url('admin/roleaccess/'). $r['id']; ?> " class="badge badge-warning">Access</a>
-                    <a href=" " class="badge badge-success">Edit</a>
-                    <a href=" " class="badge badge-danger">Delete</a>    
+                    <a href="<?= base_url('admin/roleaccess/'). $r['id']; ?> " class="badge badge-warning">Access</a>   
                     </td>
                 </tr>
                 <?php $i++; ?>
