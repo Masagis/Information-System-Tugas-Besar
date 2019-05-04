83cf05f1f9658f448c6dc8b5145f9890dc3643eb
arifthex <ariftheaxr@gmail.com>
2019-05-04 19:53:31 +0700

    update urutkan model_kelompok

--- application/models/Kelompok_model.php
+++ application/models/Kelompok_model.php
@@ -3,9 +3,15 @@
  
  class Kelompok_model extends CI_Model {
  
-    public function getKelompok(){ 
-    //mengambil data daftar mahasiswa dari table anggota   
-        return $this->db->get('user_kelompok')->result();
-     }
+    // public function getKelompok(){ 
+    // //mengambil data daftar mahasiswa dari table anggota   
+    //     return $this->db->get('user_kelompok')->result();
+    //  }
+
+    public function getKelompok()
+    {
+        $query =  $this->db->order_by('tahun,kelas,matkul','ASC') ->get('user_kelompok');
+        return $query->result();
+    }
  }
  
