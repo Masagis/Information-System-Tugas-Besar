9dfabc54e8cefc0615790e7dfa4efddce7eb6328
Doni Agus Adila <pangerancleeper@gmail.com>
2019-05-06 08:44:07 +0700

    Membuat model untuk menampilkan nilai

--- application/models/Menu_model.php
+++ application/models/Menu_model.php
@@ -10,18 +10,25 @@ class Menu_model extends CI_Model {
         return $this->db->query($query)->result_array();
     }
 
-    public function ubahSubMenu() {
-        
-    }
-    public function getSubMenuById($id)
-    {
-
-    public function ubahSubMenu() {
-        
-    }
-
     public function getSubMenuById($id){
 
         return $this->db->get_where('user_sub_menu', ['id' => $id])->row_array();
+    }
+    public function tampilnilai($id) {
+        $query = "SELECT `user_nilai_mhs`.`id`, `user`.`name`, `user_matkul`.`namamk`, `user_nilai_mhs`.`tahun_ambil`, `user_nilai_mhs`.`nilai` 
+            FROM `user_nilai_mhs`, `user`, `user_matkul`
+            WHERE `user_nilai_mhs`.`id_mhs` = `user`.`id` 
+            AND `user_nilai_mhs`.`id_matkul` = `user_matkul`.`id`
+
+        ";
+        return $this->db->query($query)->result_array();
+        #$data = $this->db->query($query)->result_array();
+        #$options = array('id' => $id );
+        #$Q = $this->db->get_where('user_nilai_mhs', $options, 1);
+        #if ($Q->num_rows() > 0) {
+        #    $data = $Q->row_array();
+        #}
+        #$Q->free_result();
+        #return $data;
     }
 }
