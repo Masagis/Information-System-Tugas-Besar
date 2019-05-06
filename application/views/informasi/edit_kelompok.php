4675ae7dd4e1da92ad24075915e294633837d3aa
Masagis <31729034+Masagis@users.noreply.github.com>
2019-05-06 20:58:29 +0700

    update view &edit view

--- application/views/informasi/edit_kelompok.php
+++ application/views/informasi/edit_kelompok.php
@@ -1,86 +1,43 @@
-<html>
-<head>
-    <title><?php echo $title ?></title>
-    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/');?>css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/');?>css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/');?>css/dataTables.bootstrap4.css">
+<!-- Begin Page Content -->
+<div class="container-fluid">
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?> Mata Kuliah <?= $v_matkul['namamk']; ?></h1>
 
-	<!-- Custom fonts for this template-->
-        <link href="<?= base_url('assets/');?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <!-- Custom styles for this template-->
-    
-
-</head>
-<body>
-<div class="container" style="margin-top: 20px">
-    <div class="row">
-        <div class="col-md-12">
-            <h2 style="text-align: center;margin-bottom: 30px">Pembagian Kelompok Tugas Besar Mahasiswa Teknik Sipil</h2>
-            <i class="fas fa-angle-double-left"></i> <a href="<?= base_url('Menu/kelompok');?>"> Back to kelompok </a>
-            <br>
-            <table id="table_id" class="table table-striped table-bordered" cellspacing="0" width="100%">
-              <thead>
+    <div class="col">
+        <div class="col-lg-8">
+        <table class="table table-hover">
+            <thead>
                 <tr>
-                    <th>No </th>
-                    <th>NIM</th>
-                    <th>Nama</th>
-                    <th>Semester</th>
-                    <th>Tahun</th>
-                    <th>Mata Kuliah 1</th>
-                    <th>Mata Kuliah 2</th>
-                    <th>Mata Kuliah 3</th>
-                    
-                <th style=width:125px;">Action
-                  </p></th>
+                <th scope="col">#</th>
+                <th scope="col">NIM</th>
+                <th scope="col">Name</th>
+                <th scope="col">Semester</th>
+                <th scope="col">Tahun Ambil</th>
+                <th scope="col">kelompok</th>
+                <th scope="col">Action</th>
                 </tr>
-              </thead>
-              <tbody>
-                    <?php 
-                        $no = 1;
-                        foreach($data_kelompok as $data){
-                    ?>
-                        <tr>
-                            <td><?php echo $no++;?></td>
-                            <td><?php echo $data->nim;?></td>
-                            <td><?php echo $data->name;?></td>
-                            <td><?php echo $data->semester;?></td>
-                            <td><?php echo $data->tahun;?></td>
-                            <td><?php echo $data->mk1;?></td>
-                            <td><?php echo $data->mk2;?></td>
-                            <td><?php echo $data->mk3;?></td>
-                            <td style="text-align: center;">
-                                <button class="btn btn-sm btn-primary" onclick="edit_book(<?php echo $data->id;?>)"><i class="fas fa-edit"></i></button>
-                                <button class="btn btn-sm btn-danger" onclick="delete_book(<?php echo $data->id;?>)"><i class="far fa-trash-alt"></i></button>
-                            </td>
-                        </tr>
-                    <?php }?>
-
-              </tbody>
+            </thead>
+            <tbody>
+            <?php $i = 1; ?>
+                <?php foreach ($data_kelompok as $n ):?>
+                <tr>
+                <th scope="row"><?= $i;?> </th>
+                <td><?= $n['nim'];?></td>
+                <td><?= $n['name'];?></td>
+                <td><?= $n['semester'];?></td>
+                <td><?= $n['tahun'];?></td>
+                <td><?= $n['kelompok'];?></td>
+                <td> <a href=" " class="badge badge-success">Edit</a> </td>
+                </tr>
+                <?php $i++; ?>
+                <?php endforeach ?>
+            </tbody>
             </table>
-        </div>
+                    </div>
+                </div>
     </div>
+    <!-- /.container-fluid -->
+
 </div>
-<script type="text/javascript" src="<?= base_url('assets/');?>js/jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/');?>js/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/');?>js/jquery.dataTables.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/');?>js/dataTables.bootstrap4.js"></script>
-<script type="text/javascript">
-  $(document).ready( function () {
-      $('#table_id').DataTable();
-  } );
-</script>
-</body>
-<footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Johnny English-Informatika <?= date('Y');?></span><br><br>
-          </div>
-        </div>
-      </footer>
-</html>
+<!-- End of Main Content --> 
--- application/views/informasi/kelompok.php
+++ application/views/informasi/kelompok.php
@@ -1,79 +1,27 @@
-<html>
-<head>
-    <title><?php echo $title ?></title>
-    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/');?>css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/');?>css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/');?>css/dataTables.bootstrap4.css">
+<!-- Begin Page Content -->
+<div class="container-fluid">
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+        <!-- Page Heading -->
+        <h1 class="h3 mb-4 text-gray-800"><?= $title; ?> Tugas Besar</h1>
+            <h3 class="h5 mb-4 text-gray-800">Daftar Tugas Besar : </h3>
+            <div class="card-columns">
+                <?php foreach ($kelas as $k ):?>
+                <div class="card mb-3" style="max-width: 500px;">
+                    <div class="row no-gutters">       
+                        <div class="col-md-8">    	
+                            <div class="card-body">
+                                <h5 class="card-title"><?= $k['namamk']; ?></h5>
+                                <p class="card-text"><?= $k['kodemk']; ?></p>
+                                <a href="<?= base_url(); ?>informasi/v_kelompok/<?= $k['kodemk']; ?>" class="badge badge-primary">View</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach ?>
+            </div>
 
-	<!-- Custom fonts for this template-->
-        <link href="<?= base_url('assets/');?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">    
-
-</head>
-<body>
-<div class="container" style="margin-top: 20px">
-    <div class="row">
-        <div class="col-md-12">
-            <h2 style="text-align: center;margin-bottom: 30px">Pembagian Kelompok Tugas Besar Mahasiswa Teknik Sipil</h2>
-            <i class="fas fa-angle-double-left"></i> <a href="<?= base_url('user');?>"> Back to home </a>
-            
-            <br>
-            <table id="table_id" class="table table-striped table-bordered" cellspacing="0" width="100%">
-              <thead>
-                <tr>
-                <th>no </th>
-                    <th>NIM</th>
-                    <th>Nama</th>
-                    <th>Semester</th>
-                    <th>Tahun</th>
-                    <th>Mata Kuliah 1</th>
-                    <th>Mata Kuliah 2</th>
-                    <th>Mata Kuliah 3</th>
-                </tr>
-              </thead>
-              <tbody>
-                    <?php 
-                        $no = 1;
-                        foreach($data_kelompok as $data){
-                    ?>
-                        <tr>
-                            <td style="text-align: center;"><?php echo $no++;?></td>
-                            <td><?php echo $data->nim;?></td>
-                            <td><?php echo $data->name;?></td>
-                            <td><?php echo $data->semester;?></td>
-                            <td><?php echo $data->tahun;?></td>
-                            <td><?php echo $data->mk1;?></td>
-                            <td><?php echo $data->mk2;?></td>
-                            <td><?php echo $data->mk3;?></td>
-                            
-                        </tr>
-                    <?php }?>
-
-              </tbody>
-            </table>
-        </div>
     </div>
+    <!-- /.container-fluid -->
+
 </div>
-<script type="text/javascript" src="<?= base_url('assets/');?>js/jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/');?>js/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/');?>js/jquery.dataTables.js"></script>
-<script type="text/javascript" src="<?= base_url('assets/');?>js/dataTables.bootstrap4.js"></script>
-<script type="text/javascript">
-  $(document).ready( function () {
-      $('#table_id').DataTable();
-  } );
-</script>
-</body>
-<footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Johnny English-Informatika <?= date('Y');?></span><br><br>
-          </div>
-        </div>
-      </footer>
-</html>
+<!-- End of Main Content -->
