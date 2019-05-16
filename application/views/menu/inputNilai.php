1cb43c344925c62422ce1f8ecfde3946354e6d0c
Masagis <31729034+Masagis@users.noreply.github.com>
2019-05-16 08:29:15 +0700

    create inputNilai 16 mei

---
+++ application/views/menu/inputNilai.php
@@ -0,0 +1,30 @@
+<div class="container">
+
+    <div class="row mt-2">
+        <div class="col-md-6">
+
+            <div class="card">
+                <div class="card-header">
+                    Input Kelompok
+                </div>
+                <div class="card-body">
+                    <form action="" method="post">
+                    <input type="hidden" name="id" value="<?= $inputAll['id']; ?>">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="name" name="name" value="<?= $inputAll['name']; ?>" readonly>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="tahun" name="tahun" value="<?= $inputAll['tahun']; ?>" readonly>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="nilai" name="nilai" value="<?= $inputAll['nilai']; ?>">
+                    </div>
+                        <button type="submit" name="ubah" class="btn btn-primary float-right">Input Kelompok</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</div>
+</div>
